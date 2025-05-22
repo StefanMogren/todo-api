f6f11dfc1d179374181d8f35808a973bf10157f2
StefanMogren <stefan_mogren@hotmail.com>
2025-05-22 10:01:02 +0200

    Finished schema model for todos in todo.js

---
+++ models/todo.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema(
+	{
+		task: {
+			type: String,
+			minlength: 6,
+			required: true,
+		},
+		done: {
+			type: Boolean,
+			required: true,
+		},
+		userId: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+export default Todo;
